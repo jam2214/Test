2ad77f924a0f4640dc24badd1bc41f4fcdfef1ac
jam2214 <janalbert.mostert@vastech.co.za>
2025-12-11 08:15:07 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,32 +1,25 @@
 import subprocess
-import requests
+from extras.scripts import Script
+from dcim.models import Device
 
-from rich import print as r_print
+class PingUpdateStatus(Script):
+    class Meta:
+        name = "Ping & Update Status"
 
-#API toke from netbox
-headers = {"Authorization": "Token 529a3b0fc593b17fd8f5f02b2211bf0908c7c26c",
-           "Content-Type": "application/json",
-           "Accept": "application/json"}
+    ip_address = "8.8.8.8"  # Hardcoded for now
 
+    def run(self, data, commit=True):
 
-ip_address = "192.168.21.216" #nexan storage unit (NXS-E18-DC-E-R3-U15-TRS)
+        # --- ping function ---
+        def is_ip_reachable(ip):
+            command = ["ping", "-c", "1", ip]  # Linux
+            return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0
 
-def is_ip_reachable(ip):
-    # Windows ping: -n 1
-    # Linux ping: -c 1
-    command = ["ping", "-n", "1", ip]
-    return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0
+        result = is_ip_reachable(self.ip_address)
 
+        # --- update NetBox object directly through ORM ---
+        device = Device.objects.get(id=39)
+        device.custom_field_data["Status"] = result
+        device.save()
 
-output = is_ip_reachable(ip_address)
-    
-if output:
-        response= requests.patch("https://10.250.11.12/api/dcim/devices/39/", headers=headers,verify=False,json={"custom_fields": {"Status": True}}) #updates only the required field to true
-        print(output)
-        r_print(response)
-        r_print(response.json())
-else:
-         response= requests.patch("https://10.250.11.12/api/dcim/devices/39/", headers=headers,verify=False,json={"custom_fields": {"Status": False}}) #updates only the required field to true   
-         print(output)
-         r_print(response)
-         r_print(response.json())
+        self.log_success(f"Ping result: {result}")
