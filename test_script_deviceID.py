c290116e1019140ee5e442e58194146a1d1063bc
jam2214 <janalbert.mostert@vastech.co.za>
2025-12-20 16:28:45 +0200

    Update test_script_deviceID.py

--- test_script_deviceID.py
+++ test_script_deviceID.py
@@ -15,9 +15,15 @@ class ShowDeviceInfo(Script):
 
     def run(self, data, commit=True):
         device = data["device"]
-        primary_ip = device.primary_ip.address if device.primary_ip else "N/A"
+
+        primary_ip = (
+            str(device.primary_ip.address).split("/")[0]
+            if device.primary_ip
+            else "N/A"
+        )
 
         self.log_success(
             f"Selected device: name={device.name}, id={device.id}, ip={primary_ip}"
         )
+
 
